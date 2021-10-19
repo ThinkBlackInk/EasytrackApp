fe4a58526415ed4db7f5258e872c45320ced192c
lgrupido <larry@think-black-ink.com>
2021-10-19 13:18:47 -0400

    Starting again - added weblink

--- www/_instantUpdateSettings.js
+++ www/_instantUpdateSettings.js
@@ -1,6 +1,6 @@
 function _instantUpdateSettings() {
 return {
-	"baseLineGUID": "82d4f221db624643bbf5c4480dea307d",
+	"baseLineGUID": "52a7c98579b04506b1f0a63c28cca03e",
 	"baseURL": "http://phonegapinstantupdate.s3.amazonaws.com/",
 	"displayMessageAfterUpdate": false,
 	"message": "",
